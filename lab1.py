b9048cc5a7a12f99b4d0db72731053ced8490451
Vova Abdrakhmanov <369565@gmail.com>
2020-09-30 12:43:06 +0400

    Use sqlalchemy

--- lab1.py
+++ lab1.py
@@ -1,25 +1,49 @@
 import requests
-import sqlite3
+from sqlalchemy import create_engine, Table, Column, String, Float, MetaData
+from sqlalchemy.sql import select
 
 url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/history'
 params = {
-    'aggregateHours':24,
-    'startDateTime':'2020-09-29T00:0:00',
-    'endDateTime':'2020-09-29T23:59:59',
-    'unitGroup':'metric',
+    'aggregateHours': 24,
+    'startDateTime': '2020-09-29T00:0:00',
+    'endDateTime': '2020-09-29T23:59:59',
+    'unitGroup': 'metric',
     'location': 'Volgograd,Russia',
     'key': 'I3D60I88UB6KPSDAVGK38HNP5',
-    'contentType':'json'
+    'contentType': 'json',
 }
 data = requests.get(url, params).json()
 
-with sqlite3.connect('weather.sqlite3') as connection:
-    c = connection.cursor()
-    c.execute('create table if not exists weather (date text, mint real, maxt real, location text, humidity real)')
-    for row in data['locations']['Volgograd,Russia']['values']:
-        c.execute('insert into weather values (?,?,?,?,?);', (row['datetimeStr'][:10], row['mint'], row['maxt'], 'Volgograd,Russia', row['humidity']))
+engine = create_engine('sqlite:///weather.sqlite3')
+metadata = MetaData()
+weather = Table(
+    'weather',
+    metadata,
+    Column('date', String),
+    Column('mint', Float),
+    Column('maxt', Float),
+    Column('location', String),
+    Column('humidity', Float),
+)
+metadata.create_all(engine)
 
-    for row in c.execute('select * from weather;'):
-        print(row)
+c = engine.connect()
 
+for row in data['locations']['Volgograd,Russia']['values']:
+    c.execute(
+        weather.insert(),
+        [
+            {
+                'date': row['datetimeStr'][:10],
+                'mint': row['mint'],
+                'maxt': row['maxt'],
+                'location': 'Volgograd,Russia',
+                'humidity': row['humidity'],
+            }
+            for row in data['locations']['Volgograd,Russia']['values']
+        ],
+    )
+
+for row in c.execute(select([weather])):
+    print(row)
 
